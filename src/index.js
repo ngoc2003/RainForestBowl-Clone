d8956941c109380ecbac35e6818aa5617e4a8b46
Bùi Thúy Ngọc <90141097+ngoc2003@users.noreply.github.com>
2022-10-23 21:16:22 +0700

    fix err vercel

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import "./index.scss";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
-import './fonts/Raleway-Medium.ttf'
+// import './fonts/Raleway-Medium.ttf'
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <BrowserRouter>
